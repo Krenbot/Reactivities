732870ab0ccf3e686dd1ee25250b01295a8e4ca6
Steve Krenn <steve.krenn@novushomemortgage.com>
2025-04-09 11:23:11 -0500

    remove unncessary boilerplate and http code

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b16a6937f82c72f8a4d8058754875438dffb1a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eb590cef5b468a02a7d83b94a3683899c24c2fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
