ec04468d22719a31eaa0e402ef2ab4995f57886d
Steve Krenn <steve.krenn@novushomemortgage.com>
2025-04-14 14:34:58 -0500

    install axios, replace fetch with axios

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61bb4d3dbfeb7876aadb10b8c8b6e3bf7d8b54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7498c4986900280ba1fbbea52ec27c0fe44d26af")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
