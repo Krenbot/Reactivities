6366a401ba6ff294508c233982ee00b42ddc00b0
Steve Krenn <steve.krenn@novushomemortgage.com>
2025-04-14 09:24:39 -0500

    expand git ignore, include API folders routes which include keys

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b407c33d917c3fb4e030b571eb35443bea41a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7091343873db3c78149cb3477ad09a88d05773")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
