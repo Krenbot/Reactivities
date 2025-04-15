b7cb2a15601db6e5fec8f236b3462ba3d844a434
Steve Krenn <steve.krenn@novushomemortgage.com>
2025-04-15 08:56:53 -0500

    add mediator handler for details

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec04468d22719a31eaa0e402ef2ab4995f57886d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7daee64068539a28d93dcab636112cec3e60a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
