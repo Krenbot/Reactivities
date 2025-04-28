339c70acb0bbcd5874586392cb93bbc428eef2fd
Steve Krenn <steve.krenn@novushomemortgage.com>
2025-04-28 16:00:12 -0500

    implemented form submission logic

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc4a540f6b446369661033e0e67a88fea18643c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5142608e5f660dd37ed7df6d0dd4825671371a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
