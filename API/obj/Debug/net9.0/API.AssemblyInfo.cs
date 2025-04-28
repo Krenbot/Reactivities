9c5142608e5f660dd37ed7df6d0dd4825671371a
Steve Krenn <steve.krenn@novushomemortgage.com>
2025-04-28 15:38:19 -0500

    set up form basics

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff48eff78e9eab5f154db16b4cebf19f441f54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc4a540f6b446369661033e0e67a88fea18643c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
