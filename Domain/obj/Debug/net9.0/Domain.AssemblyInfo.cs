852eb47fc2fe3b946aec0030f2aaddf5c22036de
Steve Krenn <steve.krenn@novushomemortgage.com>
2025-05-12 12:48:35 -0500

    sync viewed query to editing query, implement auto-date placing logic in activity edit

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d829b7ea595c4eb3d387557aeacdcb4d633bd2d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35f67aa11862d9aa5a7e5de492e34bfac3c2cdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
